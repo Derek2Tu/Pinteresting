ea0a01bddab1feabb417e0253c62e08f7492d93b
Derek Tu <yuderektu@gmail.com>
2014-11-02 01:07:48 -0400

    holy shit

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise'
+gem 'paperclip', '~> 3.0'
+
 
 group :development, :test do
 	gem 'sqlite3'
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,8 @@
 class Pin < ActiveRecord::Base
+	belongs_to :user
+
+	class Pin < ActiveRecord::Base
+     belongs_to :user
+     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	end
 end
